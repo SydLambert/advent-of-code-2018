914d4fae0fa1ef4242a2dbf95afe934b6ec354f8
Syd Lambert <syd@sydlambert.com>
2018-12-11 12:11:51 +0000

    Day 8

---
+++ day8/part1.js
@@ -0,0 +1,40 @@
+let input=require("fs").readFileSync("input1.txt","utf8").split(" ").map(e=>parseInt(e));
+
+let parse=(raw,childNodeCount)=>{
+	let childNodes=[];
+	for(let i=0;i<childNodeCount;i++){
+		if(raw[0]==0){
+			childNodes.push({
+				childNodeCount:0,
+				childNodes:[],
+				metaDataCount:raw[1],
+				metaData:raw.splice(2,raw[1]),
+			});
+			raw.splice(0,2);
+		}
+		else{
+			let newChild={
+				childNodeCount:raw[0],
+				metaDataCount:raw.splice(1,1)[0],
+			};
+			let parsed=parse(raw,raw.splice(0,1)[0]);
+			raw=parsed.raw;
+			newChild.childNodes=parsed.childNodes;
+			newChild.metaData=raw.splice(0,newChild.metaDataCount);
+			childNodes.push(newChild);
+		}
+	}
+	return {childNodes:childNodes,raw:raw};
+}
+
+let tree=parse(input,1).childNodes[0];
+
+let addMetaData=node=>{
+	let total=node.metaData.reduce((a,e)=>a+e,0);
+	node.childNodes.forEach(e=>
+		total+=addMetaData(e)
+	);
+	return total;
+}
+
+console.log(addMetaData(tree));
---
+++ day8/part2.js
@@ -0,0 +1,47 @@
+let input=require("fs").readFileSync("input2.txt","utf8").split(" ").map(e=>parseInt(e));
+
+let parse=(raw,childNodeCount)=>{
+	let childNodes=[];
+	for(let i=0;i<childNodeCount;i++){
+		if(raw[0]==0){
+			childNodes.push({
+				childNodeCount:0,
+				childNodes:[],
+				metaDataCount:raw[1],
+				metaData:raw.splice(2,raw[1]),
+			});
+			raw.splice(0,2);
+		}
+		else{
+			let newChild={
+				childNodeCount:raw[0],
+				metaDataCount:raw.splice(1,1)[0],
+			};
+			let parsed=parse(raw,raw.splice(0,1)[0]);
+			raw=parsed.raw;
+			newChild.childNodes=parsed.childNodes;
+			newChild.metaData=raw.splice(0,newChild.metaDataCount);
+			childNodes.push(newChild);
+		}
+	}
+	return {childNodes:childNodes,raw:raw};
+}
+
+let tree=parse(input,1).childNodes[0];
+
+let treeValue=node=>{
+	let total=0;
+	if(!node.childNodeCount){
+		total=node.metaData.reduce((a,e)=>a+e,0);
+	}
+	else{
+		for(let i=0;i<node.metaDataCount;i++){
+			let e=node.metaData[i]-1;
+			if(!(e in node.childNodes)) continue;
+			total+=treeValue(node.childNodes[e]);
+		}
+	}
+	return total;
+}
+
+console.log(treeValue(tree));
